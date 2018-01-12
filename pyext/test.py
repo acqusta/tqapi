499978f17cc4a6e5fb927a1a156ea19b70b5bea5
Xu Tiezhu <xutiezhu@gmail.com>
2018-01-12 21:21:27 +0800

    minor change

--- pyext/test.py
+++ pyext/test.py
@@ -13,7 +13,7 @@ def on_quote(q):
     print q['code'], q['date'], q['time'], q['last'], q['volume']
 
 dapi.set_on_quote(on_quote)
-dapi.subscribe('rb1805.SHF,J1805.DCE,000001.SH')
+dapi.subscribe('rb1805.SHF,j1805.DCE,000001.SH')
 
 while True:
     time.sleep(1)
