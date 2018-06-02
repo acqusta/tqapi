01cf7ba64412ee385efcb18421859a174a53c3c5
Xu Tiezhu <xutiezhu@gmail.com>
2018-06-02 18:48:08 +0800

    remove setup.cfg in the package

--- build_pyext.bat
+++ build_pyext.bat
@@ -1,7 +1,11 @@
 set VS90COMNTOOLS=%VS140COMNTOOLS%
 
+del setup.cfg
+python setup.py sdist
+
 copy /y setup.cfg.win32 setup.cfg
 
 python setup.py build_clib --compiler=msvc
 python setup.py build_ext --compiler=msvc
-python setup.py sdist bdist_wheel
+python setup.py bdist_wheel
+
